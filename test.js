8c3937f19c1e3936639159e5d1eafbc000dd1dbe
Danny <mr.danny951008@gmail.com>
2020-02-11 21:06:28 +0900

    extra info in test.js after push

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 console.log("Working in developer branch");
-console.log("tester branch also created . This is Tester branch ");
+console.log(
+  "tester branch also created . This is Tester branch . Plus this is extra informaint after push"
+);
